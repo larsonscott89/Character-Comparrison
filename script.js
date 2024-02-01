6cd7df2d2daeda129c79d4543ae5c44739200f5e
larsonscott89 <scottlarson0305@gmail.com>
2024-02-01 09:12:18 -0500

    added_second_textbox

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
 const apiKey = '1'
 const button = document.querySelector('#submitButton')
-const textBox = document.querySelector('#textInput')
+const textBox = document.querySelector('#ingredientInput')
+const drinkBox = document.querySelector('#textInput')
 const drinkPic = document.querySelector('.drink-picture')
 const drinkName = document.querySelector('.drink-name')
 const instructions = document.querySelector('.instructions')
@@ -17,44 +18,59 @@ const ingredientsSeven = document.querySelector('.ingredients7')
 const ingredientsEight = document.querySelector('.ingredients8')
 
 
-// button.addEventListener('click', async () => {
-//   const cocktailName = document.querySelector('#textInput').value
-//   let response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktailName}`)
+button.addEventListener('click', async () => {
+  const cocktailName = document.querySelector('#textInput').value
+  let response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktailName}`)
 
-//   let drink = response.data.drinks[0].strDrink
-//   let picture = response.data.drinks[0].strDrinkThumb
-//   let info = response.data.drinks[0].strInstructions
+  let drink = response.data.drinks[0].strDrink
+  let picture = response.data.drinks[0].strDrinkThumb
+  let info = response.data.drinks[0].strInstructions
 
-//   let ingredient1 = response.data.drinks[0].strIngredient1
-//   let ingredient2 = response.data.drinks[0].strIngredient2
-//   let ingredient3 = response.data.drinks[0].strIngredient3
-//   let ingredient4 = response.data.drinks[0].strIngredient4
-//   let ingredient5 = response.data.drinks[0].strIngredient5
-//   let ingredient6 = response.data.drinks[0].strIngredient6
-//   let ingredient7 = response.data.drinks[0].strIngredient7
-//   let ingredient8 = response.data.drinks[0].strIngredient8
+  let ingredient1 = response.data.drinks[0].strIngredient1
+  let ingredient2 = response.data.drinks[0].strIngredient2
+  let ingredient3 = response.data.drinks[0].strIngredient3
+  let ingredient4 = response.data.drinks[0].strIngredient4
+  let ingredient5 = response.data.drinks[0].strIngredient5
+  let ingredient6 = response.data.drinks[0].strIngredient6
+  let ingredient7 = response.data.drinks[0].strIngredient7
+  let ingredient8 = response.data.drinks[0].strIngredient8
 
-//   instructions.innerHTML = info
-//   drinkName.innerHTML = drink
-//   drinkPic.innerHTML = `<img src='${picture}/preview'/>`
+  instructions.innerHTML = info
+  drinkName.innerHTML = drink
+  drinkPic.innerHTML = `<img src='${picture}/preview'/>`
 
-//   ingredientsOne.innerHTML = ingredient1
-//   ingredientsTwo.innerHTML = ingredient2
-//   ingredientsThree.innerHTML = ingredient3
-//   ingredientsFour.innerHTML = ingredient4
-//   ingredientsFive.innerHTML = ingredient5
-//   ingredientsSix.innerHTML = ingredient6
-//   ingredientsSeven.innerHTML = ingredient7
-//   ingredientsEight.innerHTML = ingredient8
+  ingredientsOne.innerHTML = ingredient1
+  ingredientsTwo.innerHTML = ingredient2
+  ingredientsThree.innerHTML = ingredient3
+  ingredientsFour.innerHTML = ingredient4
+  ingredientsFive.innerHTML = ingredient5
+  ingredientsSix.innerHTML = ingredient6
+  ingredientsSeven.innerHTML = ingredient7
+  ingredientsEight.innerHTML = ingredient8
+
+  for (let button of allButtons) {
+    console.log(button.innerHTML)
+    if (button.innerHTML === '') {
+      button.style.opacity = '0'
+    } else {
+      button.style.opacity = '.95'
+    }
+  }
   
-// })
+})
+
+drinkBox.addEventListener('keydown', function (event) {
+  if (event.keyCode == 13) {
+    button.click()
+  }
+})
 
 const ingredients = Array.from({ length: 50 }, (_, index) =>
   document.querySelector(`#button${index + 1}`)
 );
 
 button.addEventListener('click', async () => {
-  const ingredientName = document.querySelector('#textInput').value;
+  const ingredientName = document.querySelector('#ingredientInput').value;
   let response = await axios.get(
     `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredientName}`
   )
@@ -79,7 +95,7 @@ button.addEventListener('click', async () => {
     if (button.innerHTML === '') {
       button.style.opacity = '0'
     } else {
-      button.style.opacity = '1'
+      button.style.opacity = '.95'
     }
   }
 })
